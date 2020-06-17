5d32efc486770c0039b1f7f6976fc493783ece5e
Shreeya Gupta <shreeyag11@gmail.com>
2020-06-17 14:51:19 -0400

    created a userItem component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import NavBar from './components/layout/NavBar';
+import UserItem from './components/users/UserItem';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <UserItem />
+      </div>
+    )
+  }
+}
+
+export default App
